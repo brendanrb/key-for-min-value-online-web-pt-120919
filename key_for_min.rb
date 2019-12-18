da5bb21fae1acfae52c38819bc5632447a0681e6
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-18 19:24:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
 name_hash.each do |key, value|
   if lowest_price == 0 || value < lowest_price
     lowest_price = value
-    lowest_key = 
+    lowest_key = key
+    
 
 end
